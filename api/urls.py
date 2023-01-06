b056b3ab2eb2d33af04eddd91c1a60790dcfe115
AxzelBC <alejo13580@gmail.com>
2023-01-06 18:19:13 -0500

    Clientes
    Exttensión de User y creación del módelo.
    Llave foranéa a User.
    Creación de la serialización.
    Definición las urls.
    Creación de las views.

--- api/urls.py
+++ api/urls.py
@@ -7,13 +7,26 @@ urlpatterns = [
 
     ##################################### URL's USUARIOS. ######################################
     path("login/", views.LoginView.as_view(), name="login"),
-    path("users/", views.UserListAll.as_view(), name="users_list_all"),
+    path("users/", views.ListUserAllView.as_view(), name="users_list_all"),
     path("users/create/", views.CreateUserView.as_view(), name="user_create"),
     path("users/view/<int:documento>",
          views.UserView.as_view(), name="user_view"),
-    path("users/modify/<int:pk>", views.UserUpdate.as_view(), name="user_modify"),
+    path("users/modify/<int:pk>",
+         views.UpdateClienteView.as_view(), name="user_modify"),
     path("users/delete/<int:documento>",
          views.UserView.as_view(), name="user_delete"),
+
+    ##################################### URL's CLIENTES. ######################################
+    path("clientes/", views.ListClientetAllView.as_view(),
+         name="clientes_list_all"),
+    path("clientes/create/", views.CreateClienteView.as_view(),
+         name="cliente_create"),
+    path("clientes/view/<int:documento>",
+         views.ClienteView.as_view(), name="cliente_view"),
+    path("clientes/modify/<int:pk>",
+         views.UpdateClienteView.as_view(), name="cliente_modify"),
+    path("clientes/delete/<int:documento>",
+         views.ClienteView.as_view(), name="cliente_delete"),
 
     ##################################### URL's AHORROS. ######################################
     # path('ahorros', Ahorros.as_view(), name='ahorros_list'),
@@ -44,13 +57,16 @@ urlpatterns = [
     path('abono/modify/<int:pk>', views.AbonoView.as_view(), name='abono_modify'),
     path('abono/delete/<int:pk>', views.AbonoView.as_view(), name='abono_modify'),
 
-     ##################################### URL's SANCIONES. ######################################
-     path('sanciones/create', views.SancionCreate.as_view(), name='sanciones_create'),
-     path('sanciones/', views.SancionList.as_view(), name='sanciones_list'),
-     path('sancion/update/<int:pk>', views.SancionUpdate.as_view(), name='sanciones_update'),
-     path('sancion/delete/<int:pk>', views.SancionDelete.as_view(), name='sanciones_delete'),
+    ##################################### URL's SANCIONES. ######################################
+    path('sanciones/create', views.SancionCreate.as_view(),
+         name='sanciones_create'),
+    path('sanciones/', views.SancionList.as_view(), name='sanciones_list'),
+    path('sancion/update/<int:pk>',
+         views.SancionUpdate.as_view(), name='sanciones_update'),
+    path('sancion/delete/<int:pk>',
+         views.SancionDelete.as_view(), name='sanciones_delete'),
 
-      ##################################### URL's Reuniones. ######################################
+    ##################################### URL's Reuniones. ######################################
     path('reuniones-presencial/create', views.ReunionPresencialCreateView.as_view(),
          name='reunionesPresencial_create'),
     path('reuniones-virtual/create', views.ReunionVirtualCreateView.as_view(),
