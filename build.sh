f25cf58f4137eeac0c11ea30334f43dfadfaf24b
Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
2023-01-24 16:01:25 -0500

    Build Fixed

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 #poetry install
-ECHO "BUILD Started"
+echo "BUILD Started"
 pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
-ECHO "Build Completed"
+echo "Build Completed"
