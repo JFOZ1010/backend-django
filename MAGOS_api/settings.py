091d5fe33e81a17a8c10274e69d8b85d754036d0
Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
2023-01-24 17:57:36 -0500

    dotenv dependency

--- MAGOS_api/settings.py
+++ MAGOS_api/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
