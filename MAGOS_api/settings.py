72828e4d68c54df5d327076537e2fa1463fefec8
Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
2023-01-24 18:12:31 -0500

    Update settings.py

--- MAGOS_api/settings.py
+++ MAGOS_api/settings.py
@@ -153,7 +153,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': '6060'
+        'PORT': '5432'
     }
 }
 
