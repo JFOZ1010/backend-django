155bb792557e71d22c91eaa9771cb2f6e9699407
Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
2023-01-24 16:11:25 -0500

    Proof 2 - port

--- MAGOS_api/settings.py
+++ MAGOS_api/settings.py
@@ -150,7 +150,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'PORT': ''
     }
 }
 
